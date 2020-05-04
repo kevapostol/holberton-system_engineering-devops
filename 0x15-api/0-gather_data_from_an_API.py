97e42b2ef35080d22612bd41cbdb4c98ac215fba
kevapostol <apostolkevin@gmail.com>
2020-05-04 23:03:25 +0000

    Refactored code

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,30 +7,24 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    # Endpoint
     uri = "https://jsonplaceholder.typicode.com"
 
-    # Making a GET request for 'USER'
     response = requests.get(uri + "/users/{}".format(argv[1]))
-
     obj = response.json()
     name = obj.get("name")
 
-    # Making a GET request to TODOS by USER_ID
     response = requests.get(uri + "/todos?userId={}".format(argv[1]))
     obj_list = response.json()
-    complete = 0
     num_tasks = len(obj_list)
 
-    # Counts the completed tasks
     completed_task = []
     for ea_todo in obj_list:
             if ea_todo.get("completed") is True:
                 completed_task.append(ea_todo)
-                complete += 1
 
-    print("Employee {} is done with tasks ({}/{}):".format(name, complete,
-          num_tasks))
+    completed_tasks = len(completed_task)
+    print("Employee {} is done with tasks ({}/{}):".format(name,
+          completed_tasks, num_tasks))
 
     # Prints the completed tasks
     for ea_todo in completed_task:
