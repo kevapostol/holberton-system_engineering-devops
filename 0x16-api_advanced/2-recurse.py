e2ba0833e7d5e51144ab7eb6202d5d2080779f2c
kevapostol <apostolkevin@gmail.com>
2020-05-07 21:44:01 +0000

    Returns a list containing the titles of all hot articles for a given subreddit

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+This module GETS data from an API
+"""
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """
+    Prints the hot posts that are on the top 10
+    Endpoint: /r/{}/hot.json?limit=10
+    """
+    uri = "https://www.reddit.com"
+    headers = {'User-Agent': 'Chrome/81.0.4044.138'}
+    new_uri = uri + "/r/{}/hot.json?limit=200&after={}".format(subreddit, after)
+    response = requests.get(new_uri, headers=headers)
+
+    if (response.status_code == 200):
+        obj = response.json()
+        posts = obj.get('data').get('children')
+
+        after = obj.get("data").get("after")
+        for ea_post in posts:
+            hot_list.append(ea_post.get("data").get("title"))
+        if after is not None:
+            recurse(subreddit, hot_list, after)
+        return hot_list
+    else:
+        return None
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # for ea_post in posts:
+    #     title = ea_post.get("data").get("title")
+    #     hot_list.append(title)
+
+    # if obj.get("after") is not None:
+    #     recurse(subreddit, hot_list, after)
+    # else:
+    #     return hot_list
